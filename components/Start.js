f2ed50b227f2c73da7d6273998332e97a7ea1574
Chriswilliams231 <70972542+Chriswilliams231@users.noreply.github.com>
2021-02-20 22:49:38 -0600

    enabled GiftedChat

--- components/Start.js
+++ components/Start.js
@@ -1,5 +1,15 @@
 import React, { Component } from 'react';
-import { StyleSheet, View, Text, TextInput, Button, ImageBackground, TouchableOpacity } from 'react-native';
+import {
+    StyleSheet,
+    View,
+    Text,
+    TextInput,
+    Button,
+    ImageBackground,
+    TouchableOpacity,
+    Platform,
+    KeyboardAvoidingView
+} from 'react-native';
 
 //import image background
 const image = require('../assets/Background-Image.png')
@@ -17,43 +27,64 @@ export default class Start extends React.Component {
             <ImageBackground source={image} style={styles.image}>
                 <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
                     <Text style={styles.title}>Start Chat</Text>
-                    <TextInput
-                        style={{ height: 40, borderColor: 'gray', borderWidth: 1, width: 250 }}
-                        onChangeText={(name) => this.setState({ name })}
-                        value={this.state.name}
-                        placeholder='Type your name ...'
-                    />
-                    <Text style={styles.text}>Choose a Background Color:</Text>
-                    <View style={styles.container}>
-                        <TouchableOpacity
-                            style={styles.color1}
-                            onPress={() => { this.setState({ color: '#090C08' }) }}
-                        >
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                            style={styles.color2}
-                            onPress={() => { this.setState({ color: '#474056' }) }}
-                        >
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                            style={styles.color3}
-                            onPress={() => { this.setState({ color: '#8A95A5' }) }}
-                        >
-                        </TouchableOpacity>
-                        <TouchableOpacity
-                            style={styles.color4}
-                            onPress={() => { this.setState({ color: '#B9C6AE' }) }}
-                        >
-                        </TouchableOpacity>
+                    <View style={styles.box}>
+                        <TextInput
+                            style={styles.textInput}
+                            onChangeText={(name) => this.setState({ name })}
+                            value={this.state.name}
+                            placeholder='Type your name ...'
+                        />
+                        <Text style={styles.text}>Choose a Background Color:</Text>
+                        <View style={styles.colorContainer}>
+                            <TouchableOpacity
+                                accessible={true}
+                                accessibilityLabel='Black background'
+                                accessibilityHint='Choose background color'
+                                accessibilityRole='button'
+                                style={styles.color1}
+                                onPress={() => { this.setState({ color: '#090C08' }) }}
+                            >
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                accessible={true}
+                                accessibilityLabel='Purple background'
+                                accessibilityHint='Choose background color'
+                                accessibilityRole='button'
+                                style={styles.color2}
+                                onPress={() => { this.setState({ color: '#474056' }) }}
+                            >
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                accessible={true}
+                                accessibilityLabel='Grey background'
+                                accessibilityHint='Choose background color'
+                                accessibilityRole='button'
+                                style={styles.color3}
+                                onPress={() => { this.setState({ color: '#8A95A5' }) }}
+                            >
+                            </TouchableOpacity>
+                            <TouchableOpacity
+                                accessible={true}
+                                accessibilityLabel='Green background'
+                                accessibilityHint='Choose background color'
+                                accessibilityRole='button'
+                                style={styles.color4}
+                                onPress={() => { this.setState({ color: '#B9C6AE' }) }}
+                            >
+                            </TouchableOpacity>
+                        </View>
+                        <Button
+                            accessible={true}
+                            accessibilityLabel='Chat Button'
+                            accessibilityHint='Navigate to chat room'
+                            accessibilityRole='button'
+                            color='#b094c2'
+                            onPress={() => {
+                                this.props.navigation.navigate('Chat', { name: this.state.name, color: this.state.color })
+                            }}
+                            title="Chat"
+                        />
                     </View>
-                    <Button
-                        color='#b094c2'
-                        onPress={() => {
-                            this.props.navigation.navigate('Chat', { name: this.state.name, color: this.state.color })
-                        }}
-                        title="Chat"
-                    />
-
                 </View>
             </ImageBackground>
         );
@@ -61,25 +92,54 @@ export default class Start extends React.Component {
 }
 
 const styles = StyleSheet.create({
-    container: {
+    colorContainer: {
         flexDirection: 'row',
         width: 200,
-        margin: 10
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: 5,
+        marginBottom: 10
+    },
+    box: {
+        flex: 1,
+        borderWidth: 2,
+        borderColor: 'transparent',
+        borderRadius: 5,
+        margin: 250,
+        padding: 10,
+        width: '88%',
+        backgroundColor: '#fff',
+        textAlign: 'center',
+
     },
     title: {
         fontSize: 45,
         fontWeight: 'bold',
-        color: '#FFFFFF'
+        color: '#FFFFFF',
+        marginTop: 10
     },
     text: {
         fontSize: 16,
         fontWeight: 'bold',
         color: '#757083',
         marginTop: 20,
+        textAlign: 'center',
         opacity: 50
     },
     image: {
         flex: 1
+    },
+    textInput: {
+        height: 50,
+        width: '90%',
+        borderWidth: 1,
+        padding: 10,
+        fontSize: 16,
+        fontWeight: '300',
+        opacity: 50,
+        marginLeft: 'auto',
+        marginRight: 'auto',
+        marginTop: 10,
     },
     color1: {
         width: 40,
